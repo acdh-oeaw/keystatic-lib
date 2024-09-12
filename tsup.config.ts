237acb52c9e600b57fbd5edcac0f3d4bb350a8e9
Stefan Probst <stefanprobst@posteo.de>
2024-09-12 10:17:27 +0200

    chore: remove uvu

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	clean: true,
 	dts: true,
-	entry: ["./src/*.ts", "!./src/*.test.ts"],
 	esbuildOptions(options) {
 		options.packages = "external";
 	},
