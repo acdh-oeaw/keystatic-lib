f5dd98f570d493247d33c721153c7788cb714d83
stefanprobst <stefanprobst@posteo.de>
2025-08-29 15:10:27 +0200

    build: include fields (#20)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
 	clean: true,
 	dts: true,
-	entry: ["./src/**/*.ts", "!./src/**/*.test.ts"],
+	entry: ["./src/**/*.ts", "./src/**/*.tsx", "!./src/**/*.test.ts"],
 	format: ["esm"],
 	minify: false,
 	sourcemap: true,
